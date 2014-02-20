1137f0f0c4627e638561566ab509f69eb15f0c17
jmlvanre <joris.van.remoortere@gmail.com>
2014-02-19 18:57:23 -0800

    Making MatDict typedef safe
    STL maps can be dangerous when the keys are pointers. Pointer to string is not what was intended here, so we're changing to std::string. Also modifying this to be an unordered_map as we don't actually care about the order.

--- TBScope/CellScope/Globals.h
+++ TBScope/CellScope/Globals.h
@@ -4,6 +4,7 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <opencv2/opencv.hpp>
 #include <stdlib.h>
+#include <unordered_map>
 
 #define DEBUG 0
 #define MODEL_PATH ""
@@ -15,7 +16,7 @@
 #define M_PI 3.1415926
 #endif
 
-typedef std::map<const char*, cv::Mat> MatDict;
+typedef std::unordered_map<std::string, cv::Mat> MatDict;
 typedef std::vector<cv::Point> ContourType;
 typedef std::vector<ContourType> ContourContainerType;
 
